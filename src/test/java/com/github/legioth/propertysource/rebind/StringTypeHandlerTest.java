d6a77c1b2f004c7fce597138cbf244ba8f00dfe5
Peter Donald <peter@realityforge.org>
2014-03-05 10:32:56 +1100

    Add first test for StringTypeHandler

---
+++ src/test/java/com/github/legioth/propertysource/rebind/StringTypeHandlerTest.java
@@ -0,0 +1,28 @@
+package com.github.legioth.propertysource.rebind;
+
+import com.google.gwt.user.rebind.StringSourceWriter;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class StringTypeHandlerTest
+{
+  @DataProvider( name = "writeValues" )
+  public Object[][] actionDescriptions()
+  {
+    return new Object[][]{
+      new Object[]{ "hi", "\"hi\"" },
+      new Object[]{ "1\n2", "\"1\\n2\"" },
+      new Object[]{ "&", "\"&\"" },
+      new Object[]{ "Å", "\"Å\"" },
+    };
+  }
+
+  @Test( dataProvider = "actionDescriptions" )
+  public void writeValues( final String input, final String output )
+  {
+    final StringSourceWriter writer = new StringSourceWriter();
+    StringTypeHandler.INSTANCE.writeValue( null, writer, input );
+    assertEquals( writer.toString(), output );
+  }
+}

6fffc21b84356cb5460b406e717e9d692ebfb730
Peter Donald <peter@realityforge.org>
2014-03-05 10:04:19 +1100

    Simplify generator by extracting out type handling for each separate type

---
+++ src/main/java/com/github/legioth/propertysource/rebind/TypeHandler.java
@@ -0,0 +1,15 @@
+package com.github.legioth.propertysource.rebind;
+
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JMethod;
+import com.google.gwt.user.rebind.SourceWriter;
+import java.util.List;
+
+interface TypeHandler<T>
+{
+  T getStaticReturnValue( TreeLogger logger, List<String> propertyValue, JMethod method )
+    throws UnableToCompleteException;
+
+  void writeValue( TreeLogger logger, SourceWriter writer, T value );
+}

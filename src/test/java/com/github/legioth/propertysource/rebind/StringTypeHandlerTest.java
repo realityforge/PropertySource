e1e60f6c518274907d2331e7b6aadac7c7dbec3d
Peter Donald <peter@realityforge.org>
2014-03-05 10:38:15 +1100

    Add some more basic tests

--- src/test/java/com/github/legioth/propertysource/rebind/StringTypeHandlerTest.java
+++ src/test/java/com/github/legioth/propertysource/rebind/StringTypeHandlerTest.java
@@ -1,8 +1,14 @@
 package com.github.legioth.propertysource.rebind;
 
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.TreeLogger.Type;
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JMethod;
 import com.google.gwt.user.rebind.StringSourceWriter;
+import java.util.Arrays;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
 public class StringTypeHandlerTest
@@ -24,5 +30,35 @@ public class StringTypeHandlerTest
     final StringSourceWriter writer = new StringSourceWriter();
     StringTypeHandler.INSTANCE.writeValue( null, writer, input );
     assertEquals( writer.toString(), output );
+  }
+
+  @Test
+  public void getStaticReturnValue()
+    throws Exception
+  {
+    final String result =
+      StringTypeHandler.INSTANCE.getStaticReturnValue( mock( TreeLogger.class ),
+                                                       Arrays.asList( "A" ),
+                                                       mock( JMethod.class ) );
+
+    assertEquals( result, "A" );
+  }
+
+  @Test
+  public void getStaticReturnValue_whenMultipleValues()
+    throws Exception
+  {
+      final TreeLogger logger = mock( TreeLogger.class );
+    try
+    {
+      StringTypeHandler.INSTANCE.getStaticReturnValue( logger,
+                                                       Arrays.asList( "A", "B" ),
+                                                       mock( JMethod.class ) );
+      fail( "Should have raised an exception" );
+    }
+    catch ( final UnableToCompleteException utce )
+    {
+      verify( logger ).log( Type.ERROR, "String only supported for properties with only one value" );
+    }
   }
 }
